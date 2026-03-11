9183d8c2ca519878c845ebf2e7604d949b7bf5d6
sohagroy7223 <freelancersohagroy@gmail.com>
2026-03-11 22:09:41 +0600

    addad signin with google success full

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -1,9 +1,25 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
+import { auth } from "../../firebase/Firebase.init";
 
 const Login = () => {
+  const provider = new GoogleAuthProvider();
+  const handelSignIn = () => {
+    console.log("google sign is clicked");
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h3>this is login page</h3>
+      <button onClick={handelSignIn}>Sign in with google</button>
     </div>
   );
 };
